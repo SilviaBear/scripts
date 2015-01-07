49be23b3573b9dda50ca53bf318a4bcc47968951
Silvia Xu <silviaxxy@gmail.com>
2015-01-07 13:09:16 -0600

    diaper

---
+++ diaper.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#cd ~
+#wget http://mir.cs.illinois.edu/gyori/sw/ed48904-diaper-junit.zip
+#unzip ed48904*
+#cd research
+#cd *
+#cd *
+#./run.sh
+#cd ~
+#./research/state-capture/diaper-junit/scripts_bash/diaperize.sh research/state-capture/diaper-junit/scripts_bash/projectsList
+cd ~
+cd projects
+for file in `find -name LOG_abba`
+do
+	echo ${file%/*}
+	echo -n "true: "
+	grep -c "true$" $file
+	echo -n "false: "
+	grep -c "false$" $file
+done
+
