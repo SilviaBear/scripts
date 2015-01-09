a7f1ad65b0b18426660dfe85ccd2f664afda2455
Silvia Xu <silviaxxy@gmail.com>
2015-01-08 20:22:46 -0600

    working version

--- diaper.sh
+++ diaper.sh
@@ -2,12 +2,16 @@
 #cd ~
 #wget http://mir.cs.illinois.edu/gyori/sw/ed48904-diaper-junit.zip
 #unzip ed48904*
-#cd research
-#cd *
-#cd *
-#./run.sh
-#cd ~
-#./research/state-capture/diaper-junit/scripts_bash/diaperize.sh research/state-capture/diaper-junit/scripts_bash/projectsList
+cd ~/research
+cd *
+cd *
+./run.sh
+cd ~
+#./research/state-capture/diaper-junit/scripts_bash/diaperize.sh research/state-capture/diaper-junit/scripts_bash/4projects >> badresult.txt
+#wait
+#cd projects
+./research/state-capture/diaper-junit/scripts_bash/diaperize.sh research/state-capture/diaper-junit/scripts_bash/goodguy >>goodguy.txt
+wait
 cd ~
 cd projects
 for file in `find -name LOG_bbaa`
--- ekstazi@zookeeper.sh
+++ ekstazi@zookeeper.sh
@@ -1,23 +1,37 @@
 #!/bin/bash
+function insert(){
+	sed -i '/<project name=*/ a xmlns:ekstazi="antlib:org.ekstazi.ant"' build.xml
+	sed -i '/<\/project>/i <taskdef uri="antlib:org.ekstazi.ant" resource="org/ekstazi/ant/antlib.xml">\
+	<classpath path="org.ekstazi.core-4.5.2.jar"/>\
+	<classpath path="org.ekstazi.ant-4.5.2.jar"/>\
+	</taskdef>' build.xml
+	sed -i '/<junit*/i <ekstazi:select>' build.xml
+	sed -i '/<\/junit>/a </ekstazi:select>' build.xml
+}
 cd ~
+rm -rf zookeeper
 git clone https://github.com/apache/zookeeper
 cd zookeeper
 wget http://www.ekstazi.org/release/org.ekstazi.core-4.5.2.jar
 wget http://www.ekstazi.org/release/org.ekstazi.ant-4.5.2.jar
-rev2=`git log | grep "^commit " | head -n 1 | cut -c 8-14`
-rev1=`git log | grep "^commit " | head -n 2 | tail -n 1 | cut -c 8-14`
+rev1=`git log | grep "^commit " | head -n 3 | tail -n 1 | cut -c 8-14`
+rev2=`git log | grep "^commit " | head -n 2 | tail -n 1| cut -c 8-14`
 git reset --hard "${rev1}"
-sed -i '/<project name=*/ a xmlns:ekstazi="antlib:org.ekstazi.ant"' build.xml
-sed -i '/<\/project>/i <taskdef uri="antlib:org.ekstazi.ant" resource="org/ekstazi/ant/antlib.xml">\
-<classpath path="org.ekstazi.core-4.5.2.jar"/>\
-<classpath path="org.ekstazi.ant-4.5.2.jar"/>\
-</taskdef>' build.xml
-sed -i '/<junit*/i <ekstazi:select>' build.xml
-sed -i '/<\/junit>/a </ekstazi:select>' build.xml
+insert
+echo "dependencies.ignored.paths = /Info" >> .ekstazirc
 ant test >> output${rev1}.txt
+wait
 ant test >> output${rev1}again.txt
-echo "Test run for ${rev1}:"
+wait
+git reset --hard "${rev2}"
+insert
+ant test >> output${rev2}.txt
+wait
+echo "Result for ${rev1}:"
 grep -c "[junit]" output${rev1}.txt
-echo "Test run for ${rev1} again:"
+echo "Result for ${rev1} again:"
 grep -c "[junit]" output${rev1}again.txt
+echo "Result for ${rev2}:"
+grep -c "[junit]" output${rev2}.txt
+
 
